89973af7b2b0cf8aa182e6ae1758dda92367fd15
asjadahamed77 <asjadahamed77@gmail.com>
2025-01-02 09:58:54 +0530

    Created Pages and Mounted it to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Brands from './pages/Brands';
+import Perfumes from './pages/Perfumes';
+import UserProfile from './pages/UserProfile';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import Cart from './pages/Cart';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/brands" element={<Brands />} />
+        <Route path="/perfumes" element={<Perfumes />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Perfumes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Perfumes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Perfumes
